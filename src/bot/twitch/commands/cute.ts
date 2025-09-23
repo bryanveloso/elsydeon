5af0494a51e6f8509cecdfa8efd187d1878288cf
Bryan Veloso <bryan@velo.so>
2025-09-22 18:13:53 -0700

    Support multi-word arguments for !cute.

--- src/bot/twitch/commands/cute.ts
+++ src/bot/twitch/commands/cute.ts
@@ -1,8 +1,8 @@
 import { createBotCommand } from '@twurple/easy-bot'
 
 export const cute = createBotCommand('cute', (params, { msg: { userInfo }, say }) => {
-  // Use the first parameter as the username, or default to the user who called the command
-  const username = params[0] || userInfo.displayName
+  // Join all parameters for multi-word phrases, or default to the user who called the command
+  const username = params.length > 0 ? params.join(' ') : userInfo.displayName
 
   if (username.toLowerCase() === 'elsydeon') {
     say('avalonREVERSE')
