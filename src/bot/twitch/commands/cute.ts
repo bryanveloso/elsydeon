9608d3ce774175fe9faa5ea95f51356ba50247be
Bryan Veloso <bryan@velo.so>
2025-09-22 18:05:37 -0700

    Bot doesn't want to be called cute.

--- src/bot/twitch/commands/cute.ts
+++ src/bot/twitch/commands/cute.ts
@@ -4,5 +4,10 @@ export const cute = createBotCommand('cute', (params, { msg: { userInfo }, say }
   // Use the first parameter as the username, or default to the user who called the command
   const username = params[0] || userInfo.displayName
 
+  if (username.toLowerCase() === 'elsydeon') {
+    say('avalonREVERSE')
+    return
+  }
+
   say(`${username} is cute, pass it on.`)
 })
