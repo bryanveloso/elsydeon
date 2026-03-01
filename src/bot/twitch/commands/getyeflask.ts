ab65d7f79bd3e7469719bdd115828081603086d9
Bryan Veloso <bryan@velo.so>
2026-03-01 13:57:37 -0800

    Remove !getyeflask command.

--- src/bot/twitch/commands/getyeflask.ts
+++
@@ -1,5 +0,0 @@
-import { createBotCommand } from '@twurple/easy-bot'
-
-export const getyeflask = createBotCommand('getyeflask', (params, { msg: { userInfo }, say }) => {
-  say(`WorldFriendshopBot didn't leave me any flasks in their will, ${userInfo.displayName} bardSadge `)
-})
--- src/bot/twitch/commands/index.ts
+++ src/bot/twitch/commands/index.ts
@@ -4,7 +4,6 @@ import { log } from '@core/utils/logger'
 
 // Core commands
 import { cute } from './cute'
-import { getyeflask } from './getyeflask'
 import { punt } from './punt'
 import { slap } from './slap'
 
@@ -39,7 +38,7 @@ const commandList: BotCommand[] = []
 
 // Core commands
 if (features.core) {
-  commandList.push(cute, getyeflask, punt, slap)
+  commandList.push(cute, punt, slap)
 }
 
 // Quote system
