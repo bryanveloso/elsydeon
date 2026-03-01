4278878fe9178405e9aae8d28d0ac7f4825882bf
Bryan Veloso <bryan@velo.so>
2026-02-28 20:22:32 -0800

    Make !getyeflask reflect the old command a little.

--- src/bot/twitch/commands/getyeflask.ts
+++ src/bot/twitch/commands/getyeflask.ts
@@ -1,5 +1,5 @@
 import { createBotCommand } from '@twurple/easy-bot'
 
-export const getyeflask = createBotCommand('getyeflask', (_params, { say }) => {
-  say(`This command is currently under construction. You get a flask. There's nothing in it. It's a supply chain issue.`)
+export const getyeflask = createBotCommand('getyeflask', (params, { msg: { userInfo }, say }) => {
+  say(`WorldFriendshopBot didn't leave me any flasks in their will, ${userInfo.displayName} :(`)
 })
